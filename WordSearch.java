7bdcb42f6abcc2d0e75b7d2b87c982cff3c2ccf7
lizeyliz <alizaeaton@gmail.com>
2024-10-16 16:43:32 -0700

    setting up WordSearch object class

---
+++ WordSearch.java
@@ -0,0 +1,11 @@
+public class WordSearch {
+    //initialize variables
+    private String word1;
+    private String word2;
+    private String word3;
+    private String word4;
+    private String word5; 
+
+    //constructor
+    public WordSearch
+}
