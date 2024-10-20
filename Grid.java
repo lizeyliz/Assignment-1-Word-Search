c6910f52d2f39b5c41e32446caf68051a3ad3e53
lizeyliz <alizaeaton@gmail.com>
2024-10-20 16:34:38 -0700

    moved wordSearchTest code to Grid and deleted

--- Grid.java
+++ Grid.java
@@ -1,9 +1,28 @@
+// Citation:
+// Multi-Dimensional Arrays: https://www.w3schools.com/java/java_arrays_multi.asp
+// Method Calls: https://www.w3schools.com/java/java_class_methods.asp 
+// For-Each Loops: https://www.w3schools.com/java/java_foreach_loop.asp
+// Google AI search "print grid in Java"
+// Google AI search "initialize multidimensional array to size java"
+
 import java.security.SecureRandom;
+import java.util.Scanner;
 
 public class Grid {
     public static void main(String[] args) {
         //instance of SecureRandom (object)
         SecureRandom secureRandom = new SecureRandom();
+
+        //initialize
+        int userInt = 0;
+        Scanner scan = new Scanner(System.in);
+        //loop user menu
+        /*do {  
+            userMenu(userInt);
+            //get user input
+            userInt = scan.nextInt();
+        } while (userInt != 4);
+        System.out.println("Goodbye!");*/
 
         //initialize words
         String word1 = "crane";
@@ -13,8 +32,8 @@ public class Grid {
         String word5 = "crazy";
 
         //initialize wordSearch array to rows x columns
-        int rows = 2;
-        int columns = 5;
+        int rows = 20;
+        int columns = 20;
         char wordSearch[][] = new char[rows][columns];
         //populate array wordSearch[][] with xs
         for (int row = 0; row < rows; row++) {
@@ -141,9 +160,33 @@ public class Grid {
             }//end inner for loop
             System.out.println();
         }//end outer for loop
-
-
     }//end printWordSearch method
+
+    //user menu
+    public static void userMenu(int userInt){
+        switch (userInt) {
+            case 1 -> {
+                System.out.println("create a wordsearch (method)");
+            }
+            case 2 -> {
+                System.out.println("view wordsearch with solution");
+            }
+            case 3 -> {
+                System.out.println("view wordsearch without solutions");
+            }
+            case 4 -> {
+                //quit
+            }
+            default -> {
+                System.out.println("Enter a number between 1 and 4:");
+            } 
+        } //end switch/case
+        //print user menu
+        System.out.println("1) Create a wordsearch");
+        System.out.println("2) View a wordsearch with solutions");
+        System.out.println("3) View a wordsearch without solutions");
+        System.out.println("4) Quit");
+    } // end method userMenu
 }//end class
 
 //To do:
--- WordSearchTest.java
+++ WordSearchTest.java
@@ -1,12 +1,5 @@
-// Citation:
-// Multi-Dimensional Arrays: https://www.w3schools.com/java/java_arrays_multi.asp
-// Method Calls: https://www.w3schools.com/java/java_class_methods.asp 
-// For-Each Loops: https://www.w3schools.com/java/java_foreach_loop.asp
-// Google AI search "print grid in Java"
-// Google AI search "initialize multidimensional array to size java"
-import java.util.Scanner;
-//this class currently only contains user menu
-public class WordSearchTest {
+
+/*public class WordSearchTest {
     public static void main(String[] args) {
         //initialize
         int userInt = 0;
@@ -19,29 +12,4 @@ public class WordSearchTest {
         } while (userInt != 4);
         System.out.println("Goodbye!");
     } // end main method
-    //user menu
-    public static void userMenu(int userInt){
-        switch (userInt) {
-            case 1 -> {
-                System.out.println("create a wordsearch (method)");
-            }
-            case 2 -> {
-                System.out.println("view wordsearch with solution");
-            }
-            case 3 -> {
-                System.out.println("view wordsearch without solutions");
-            }
-            case 4 -> {
-                //quit
-            }
-            default -> {
-                System.out.println("Enter a number between 1 and 4:");
-            } 
-        } //end switch/case
-        //print user menu
-        System.out.println("1) Create a wordsearch");
-        System.out.println("2) View a wordsearch with solutions");
-        System.out.println("3) View a wordsearch without solutions");
-        System.out.println("4) Quit");
-    } // end method userMenu
-}
+}*/
