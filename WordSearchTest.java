582893e4d4e76585a138535651b16e45a2bec5b1
lizeyliz <alizaeaton@gmail.com>
2024-10-20 16:28:20 -0700

    cleaned up class WordSearchTest (will merge with grid later)

--- WordSearchTest.java
+++ WordSearchTest.java
@@ -4,64 +4,23 @@
 // For-Each Loops: https://www.w3schools.com/java/java_foreach_loop.asp
 // Google AI search "print grid in Java"
 // Google AI search "initialize multidimensional array to size java"
-import java.security.SecureRandom;
 import java.util.Scanner;
+//this class currently only contains user menu
 public class WordSearchTest {
     public static void main(String[] args) {
         //initialize
         int userInt = 0;
         Scanner scan = new Scanner(System.in);
-        //for calling methods, will change later
-        WordSearchTest myWordSearch = new WordSearchTest();
-        //test
-        //myWordSearch.createWordSearch();
-
-        //bringing in random ints
-        SecureRandom secureRandom = new SecureRandom();
-        // Generate a random integer between 1 (inclusive) and 15 (exclusive)
-        int randomInt = secureRandom.nextInt(15) + 1; 
-        System.out.println(randomInt);
-
-        //initialize two dimensional array grid
-        char[][] grid = new char[20][20];
-        // Initialize grid elements to x
-
-        for (int x = 0; x < 20; x++) { //rows
-            for (int y = 0; y < 20; y++) { //columns
-                grid[y][x] = 'x';
-            }
-        }
-        //add word 1 to grid vertically (loc 1 to 5)
-        //have to add space after words (maybe fix this later)
-        //add yourself, don't make user add space
-        String word1 = "crane ";
-        //test
-        //y,x
-        grid[0][0] = word1.charAt(0);
-        grid[0][1] = word1.charAt(1); 
-
-
-
-
-
-        //print grid
-        for (char[] row : grid) {
-            for (char cell : row) {
-                System.out.print(cell + " ");
-            }
-            System.out.println();
-        }
-
         //loop user menu
-        /*do {  
-            myWordSearch.userMenu(userInt);
+        do {  
+            userMenu(userInt);
             //get user input
             userInt = scan.nextInt();
         } while (userInt != 4);
-        System.out.println("Goodbye!");*/
+        System.out.println("Goodbye!");
     } // end main method
     //user menu
-    public void userMenu(int userInt){
+    public static void userMenu(int userInt){
         switch (userInt) {
             case 1 -> {
                 System.out.println("create a wordsearch (method)");
@@ -85,38 +44,4 @@ public class WordSearchTest {
         System.out.println("3) View a wordsearch without solutions");
         System.out.println("4) Quit");
     } // end method userMenu
-
-    //create a wordsearch
-    //make sure you have different directions
-    public String createWordSearch() {
-        //looking up print in grid java, use two dimensional array
-        //e.g char word[x][y] = 'c' location of char
-        String wordSearch = "x x x x x"; //times 5 (newline)
-        //test words, will add user input later
-        String word1 = "crane";
-        String word2 = "place";
-        String word3 = "times";
-        String word4 = "happy";
-        String word5 = "crazy";
-        //array of chars
-        char[] allChars = new char[24];
-        //array of words
-        String[] words = {word1, word2, word3, word4, word5};
-        //word 1 char array
-        char[] word1Chars = new char[5];
-        for (int i = 0; i < 5; i++) {
-            word1Chars[i]= word1.charAt(i);
-        }
-        //TESTING print word1Chars
-        for (int i = 0; i < 5; i++) {
-            System.out.println(word1Chars[i]);
-        }
-        //horizontal: x+1, y for each increment
-        //vertical: x, y-1, for each increment
-        return wordSearch;
-    }
-
-    //view a wordsearch with solutions
-
-    //view a wordsearch without solutions
 }
