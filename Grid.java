2dde0ad2e934bf66294606efc14190140d61a99b
lizeyliz <alizaeaton@gmail.com>
2024-10-19 19:04:24 -0700

    cleaning up

--- Grid.java
+++ Grid.java
@@ -4,53 +4,41 @@ public class Grid {
     public static void main(String[] args) {
         //instance of SecureRandom (object)
         SecureRandom secureRandom = new SecureRandom();
-        //create random number between 0 and
-        int rowInt = secureRandom.nextInt(16);
-        int columnInt = secureRandom.nextInt(16);
-        //print random number for test purposes
-        System.out.println("Row int: " + rowInt);
-        System.out.println("Column int: " + columnInt);
-
-        //initialize
+        //initialize ints
         int rows = 25;
         int columns = 25;
+        int rowInt = secureRandom.nextInt(16);//between 0-15
+        int columnInt = secureRandom.nextInt(16);//between 0-15
+        //initialize words
+        String word1 = "crane";
+
         //initialize wordSearch array to rows x columns
         char wordSearch[][] = new char[rows][columns];
-
-        //populate array wordSearch[][]
+        //populate array wordSearch[][] with xs
         for (int row = 0; row < rows; row++) {
             for (int column = 0; column < columns; column++) {
                 wordSearch[row][column] = 'x';
             }//end inner for loop
         }//end outer for loop
 
-        //test: will print in row 1, column 2 (counting from zero)
-        //because of the way loops work, has to be this way
-        //wordSearch[1][2] = '!';
+        //print for test purposes
+        System.out.println("Row int: " + rowInt);
+        System.out.println("Column int: " + columnInt);
 
-        //print word1 horizontally
-        String word1 = "crane";
-        //rows, columns
-        //wordSearch[0][0] = word1.charAt(0);
-        //wordSearch[0][1] = word1.charAt(1);
-
-        //loop to print crane horizontally in row 0
-        
-        //rows: should stay the same
-        int index = 0;
-        for (int j = columnInt; j < columnInt + 5; j++) {//columns: should increase
+        //print word1 horizontally in a random location
+        //rows: stay the same, columns: increase
+        int index = 0; //index in string
+        for (int j = columnInt; j < columnInt + 5; j++) {
             wordSearch[rowInt][j] = word1.charAt(index);
-            index++;
+            index++;//increasing so we can go through word
         }
 
-
-        //Test: print wordSearch[][] grid
+        //print wordSearch[][] grid
         for (int row = 0; row < rows; row++) {
             for (int column = 0; column < columns; column++) {
                 System.out.print(wordSearch[row][column] + " ");
             }//end inner for loop
             System.out.println();
         }//end outer for loop
-        
     }
 }
