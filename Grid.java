f13116b672edd85722178a7804d2557f173a3b5a
lizeyliz <alizaeaton@gmail.com>
2024-10-19 19:30:13 -0700

    cleaning up again

--- Grid.java
+++ Grid.java
@@ -4,14 +4,14 @@ public class Grid {
     public static void main(String[] args) {
         //instance of SecureRandom (object)
         SecureRandom secureRandom = new SecureRandom();
-        //initialize ints
-        int rows = 25;
-        int columns = 25;
+
         //initialize words
         String word1 = "crane";
         String word2 = "place";
 
         //initialize wordSearch array to rows x columns
+        int rows = 25;
+        int columns = 25;
         char wordSearch[][] = new char[rows][columns];
         //populate array wordSearch[][] with xs
         for (int row = 0; row < rows; row++) {
@@ -32,15 +32,16 @@ public class Grid {
         }//end outer for loop
     }//end main method
 
-    //place word horizontally in a random location
+    //place word horizontally in a random location (make sure it fits page)
     public static char[][] placeHorizontally(char[][] wordSearch, SecureRandom secureRandom, String word) {
+        //create random ints
         int rowInt = secureRandom.nextInt(16);//between 0-15
         int columnInt = secureRandom.nextInt(16);//between 0-15
-
         //print for test purposes
         System.out.println("Row int: " + rowInt);
         System.out.println("Column int: " + columnInt);
 
+        //place word horizontally
         //rows: stay the same, columns: increase
         int index = 0; //index in string
         for (int j = columnInt; j < columnInt + 5; j++) {
@@ -50,6 +51,7 @@ public class Grid {
         return wordSearch;
     }//end method placeHorizontally
 }//end class
+
 //To do:
 // - make sure words don't overlap: if does NOT contain'-', don't put word there
 //  (add and check again?) won't work if you're putting random letters there
